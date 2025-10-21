bffcff6bcf9b92fa4c0fdf42848f0d90150dea86
123home <123home@DESKTOP-RR794IQ>
2025-10-21 21:11:55 +0500

    Изменил тесты и поправил функцию ещё раз

--- test_main.py
+++ test_main.py
@@ -2,7 +2,6 @@ from main import port_survival_stat
 import pandas as pd
 
 def test_type_of_survival_stat():
-    # arrange
     port = "Саутгемптон"
     data = pd.DataFrame({
         'Survived': [1, 0, 1, 1],
@@ -11,14 +10,11 @@ def test_type_of_survival_stat():
     })
     expected = type(pd.DataFrame())
 
-    # act
     actual = type(port_survival_stat(port, data))
 
-    # assert
     assert expected == actual
 
 def test_columns_survival_stat():
-    # arrange
     port = "Саутгемптон"
     data = pd.DataFrame({
         'Survived': [1, 0, 1, 1],
@@ -27,14 +23,11 @@ def test_columns_survival_stat():
     })
     expected = ["Спасенные", "Умершие"]
 
-    # act
     actual = list(port_survival_stat(port, data).columns)
 
-    # assert
     assert expected == actual
 
 def test_right_port_survival_stat():
-    # arrange
     port = "Саутгемптон"
     data = pd.DataFrame({
         'Survived': [1, 0, 1, 1],
@@ -46,14 +39,41 @@ def test_right_port_survival_stat():
         "Умершие" : [1]
     })
 
-    # act
     actual = port_survival_stat(port, data)
 
-    # assert
     assert expected.equals(actual)
 
+
+def test_multiple_ports():
+    first_port = "Саутгемптон"
+    second_port = "Шербур"
+    third_port = "Квинстаун"
+    data = pd.DataFrame({
+        'Survived': [1, 0, 1, 1, 0, 0],
+        'Embarked': ["S", "S", "C", "C", "Q", "Q"]
+    })
+    first_expected = pd.DataFrame({
+        "Спасенные" :[1],
+        "Умершие" : [1]
+    })
+    second_expected = pd.DataFrame({
+        "Спасенные" :[2],
+        "Умершие" : [0]
+    })
+    third_expected =pd.DataFrame({
+        "Спасенные" :[0],
+        "Умершие" : [2]
+    })
+
+    first_actual = port_survival_stat(first_port, data)
+    second_actual = port_survival_stat(second_port, data)
+    third_actual = port_survival_stat(third_port, data)
+
+    assert first_expected.equals(first_actual)
+    assert second_expected.equals(second_actual)
+    assert third_expected.equals(third_actual)
+
 def test_wrong_port_survival_stat():
-    # arrange
     port = "Лондон"
     data = pd.DataFrame({
         'Survived': [1, 0, 1, 1],
@@ -65,8 +85,6 @@ def test_wrong_port_survival_stat():
         "Умершие": [0]
     })
 
-    # act
     actual = port_survival_stat(port,data)
 
-    # assert
     assert expected.equals(actual)
